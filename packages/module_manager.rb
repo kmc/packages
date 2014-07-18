854368d2b296935fdf8d6649025a580f681f6b59
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-18 20:03:26 +0200

    Update Module Manager to 2.2.0 (KSP 0.24)

--- packages/module_manager.rb
+++ packages/module_manager.rb
@@ -1,9 +1,9 @@
 class ModuleManager < Kmc::Package
   title 'ModuleManager'
   aliases 'module manager'
-  url 'https://ksp.sarbian.com/jenkins/job/ModuleManager/lastBuild/artifact/jenkins-ModuleManager-36/ModuleManager-2.1.5.zip'
+  url 'https://ksp.sarbian.com/jenkins/job/ModuleManager/39/artifact/jenkins-ModuleManager-39/ModuleManager-2.2.0.zip'
 
   def install
-    merge_directory 'ModuleManager.2.1.5.dll', into: 'GameData'
+    merge_directory 'ModuleManager.2.2.0.dll', into: 'GameData'
   end
 end
