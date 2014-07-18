adc7dcf28bdfdb6b8f77a7876b4954afa0509473
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-18 19:53:58 +0200

    Update Kerbal Alarm Clock to 2.7.7.0 (KSP 0.24)

--- packages/kerbal_alarm_clock.rb
+++ packages/kerbal_alarm_clock.rb
@@ -1,8 +1,8 @@
 class KerbalAlarmClock < Kmc::Package
   title 'Kerbal Alarm Clock'
-  url 'https://github.com/TriggerAu/KerbalAlarmClock/releases/download/v2.7.6.0/KerbalAlarmClock_2.7.6.0.zip'
+  url 'https://github.com/TriggerAu/KerbalAlarmClock/releases/download/v2.7.7.0/KerbalAlarmClock_2.7.7.0.zip'
 
   def install
-    merge_directory 'KerbalAlarmClock_2.7.6.0/GameData'
+    merge_directory 'KerbalAlarmClock_2.7.7.0/GameData'
   end
 end
