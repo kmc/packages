19c371394308ee83118f230f37a7ff45ff9d3723
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-18 20:15:21 +0200

    Update Toolbar to 1.7.4 (KSP 0.24)

--- packages/toolbar.rb
+++ packages/toolbar.rb
@@ -1,9 +1,9 @@
 class Toolbar < Kmc::Package
   title 'Toolbar Plugin'
   aliases 'toolbar'
-  url 'http://blizzy.de/toolbar/Toolbar-1.7.3.zip'
+  url 'http://blizzy.de/toolbar/Toolbar-1.7.4.zip'
 
   def install
-    merge_directory 'Toolbar-1.7.3/GameData'
+    merge_directory 'Toolbar-1.7.4/GameData'
   end
 end
