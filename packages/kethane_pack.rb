ce348e71b640e16e7764a11870e453b592b2a342
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-18 20:08:50 +0200

    Update Kethane to 0.8.8 (KSP 0.24)

--- packages/kethane_pack.rb
+++ packages/kethane_pack.rb
@@ -1,7 +1,7 @@
 class KethanePack < Kmc::Package
   title 'Kethane Pack'
   aliases 'kethane'
-  url 'https://nabaal.net/files/kethane/Kethane-0.8.6.zip'
+  url 'https://nabaal.net/files/kethane/Kethane-0.8.8.zip'
 
   def install
     merge_directory 'GameData'
