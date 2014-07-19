b7cf22123dda7968080b2fc401af86605bd0189b
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-19 11:40:36 +0200

    Update Deadly Reentry to 4.8 (KSP 0.24)

--- packages/deadly_reentry.rb
+++ packages/deadly_reentry.rb
@@ -1,9 +1,9 @@
 class DeadlyReentry < Kmc::Package
   title 'Deadly Reentry'
-  url 'https://github.com/NathanKell/DeadlyReentry/releases/download/v4.7/DeadlyReentryCont_v4.7.zip'
+  url 'https://github.com/NathanKell/DeadlyReentry/releases/download/v4.8/DeadlyReentryCont_v4.8.zip'
+  prerequisites 'module-manager'
 
   def install
     merge_directory 'DeadlyReentry', into: 'GameData'
-    merge_directory 'ModuleManager.2.1.0.dll', into: 'GameData'
   end
 end
