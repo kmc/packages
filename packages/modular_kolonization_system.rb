11fd4c497a8f3cd6077b4a8a19c48f2316c5f73b
Dan Keller <dan@dankeller.org>
2014-07-22 10:52:29 -0500

    Update modular_kolonization_system.rb
    version .18.8

--- packages/modular_kolonization_system.rb
+++ packages/modular_kolonization_system.rb
@@ -1,6 +1,6 @@
 class ModularKolonizationSystem < Kmc::Package
   title 'Modular Kolonization System'
-  url 'https://www.dropbox.com/sh/1fsuzvl35s2gppt/AACiDPCa7ZNPhrNIYWCmGXRea/MKS_0.17.0.zip'
+  url 'https://www.dropbox.com/sh/1fsuzvl35s2gppt/AABOTMlUqHkOUYSxQ41jykPLa/MKS_0.18.8.zip'
 
   def install
     merge_directory 'GameData'
