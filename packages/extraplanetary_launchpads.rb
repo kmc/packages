f8f2a40bc3798f780e6486399c9431cdbf3f5232
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-22 18:17:44 +0200

    Update Extraplanetary Launchpads to 4.2.2 (KSP 0.24)

--- packages/extraplanetary_launchpads.rb
+++ packages/extraplanetary_launchpads.rb
@@ -1,7 +1,7 @@
 class ExtraplanetaryLaunchpads < Kmc::Package
   title 'Extraplanetary Launchpads'
   aliases 'extraplanetary-launchpads'
-  url 'http://taniwha.org/~bill/Extraplanetary_Launchpads_v4.2.1.zip'
+  url 'http://taniwha.org/~bill/Extraplanetary_Launchpads_v4.2.2.zip'
 
   def install
     merge_directory 'ExtraplanetaryLaunchpads', into: 'GameData'
