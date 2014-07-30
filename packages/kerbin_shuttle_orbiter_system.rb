3693a18310ed9448762f0b769c98e39c2858d233
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-30 16:47:52 +0200

    Update Kerbin Shuttle Orbiter System to 3.11

--- packages/kerbin_shuttle_orbiter_system.rb
+++ packages/kerbin_shuttle_orbiter_system.rb
@@ -1,11 +1,12 @@
 class KerbinShuttleOrbiterSystem < Kmc::Package
   title 'Kerbin Shuttle Orbiter System'
   aliases 'kerbin-shuttle-orbiter-system', 'kso'
-  url 'http://www.mediafire.com/download/x3j4kjml2mlnxvy/KSOS_309hf.zip'
+  url 'http://www.mediafire.com/download/2898v27i7na9li7/KSOS_v311hf.zip'
   prerequisites 'Active Texture Management'
 
   def install
-    merge_directory 'KSOS_v309hf/GameData'
+    merge_directory 'KSOS_v311hf/GameData'
+    merge_directory 'KSOS_v311hf/Ships'
   end
 end
 
