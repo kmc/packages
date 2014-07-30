889700476a4d704bfdab86332ce26b3c953b12a6
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-30 23:51:08 +0200

    Update Alternate Resource Panel to 2.4.3.0

--- packages/alternate_resource_panel.rb
+++ packages/alternate_resource_panel.rb
@@ -1,9 +1,9 @@
 class AlternateResourcePanel < Kmc::Package
   title 'KSP Alternate Resource Panel'
   aliases 'alternate-resource-panel', 'arp'
-  url 'https://github.com/TriggerAu/AlternateResourcePanel/releases/download/v2.4.2.0/KSPAlternateResourcePanel_2.4.2.0.zip'
+  url 'https://github.com/TriggerAu/AlternateResourcePanel/releases/download/v2.4.3.0/KSPAlternateResourcePanel_2.4.3.0.zip'
 
   def install
-    merge_directory 'KSPAlternateResourcePanel_2.4.2.0/GameData'
+    merge_directory 'KSPAlternateResourcePanel_2.4.3.0/GameData'
   end
 end
