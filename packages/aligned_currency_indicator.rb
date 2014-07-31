2b221a2f0a41b1b79935f7024bdf43ea3ba2d1d7
Robin Appelman <icewind@owncloud.com>
2014-07-31 19:42:08 +0200

    add Aligned Currency Indicator

---
+++ packages/aligned_currency_indicator.rb
@@ -0,0 +1,9 @@
+class AlignedCurrencyIndicator < Kmc::Package
+  title 'Aligned Currency Indicator'
+  url 'https://bitbucket.org/xEvilReeperx/ksp_alignedcurrencyindicator/downloads/KSP-0.24.1-AlignedCurrency-1.1.zip'
+
+  def install
+    merge_directory 'GameData'
+  end
+end
+
