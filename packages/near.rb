13c36e1306e13d5c3a927780e82a80bbc49b8eb5
Robin Appelman <icewind@owncloud.com>
2014-07-31 19:33:43 +0200

    update NEAR to 1.1.1

--- packages/near.rb
+++ packages/near.rb
@@ -1,7 +1,7 @@
 class NEAR < Kmc::Package
   title 'Neophytes Elementary Aerodynamics Replacement'
   aliases 'NEAR'
-  url 'https://github.com/ferram4/Ferram-Aerospace-Research/releases/download/NEAR_v1.0.2/NEAR_v1_0_2.zip'
+  url 'https://github.com/ferram4/Ferram-Aerospace-Research/releases/download/NEAR_v1.1.1/NEAR_v1_1_1.zip'
 
   def install
     merge_directory 'GameData'
