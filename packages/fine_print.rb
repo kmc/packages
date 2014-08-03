fc72ed6eac7fc44cb00e745b4945cc2c53ed68a0
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-03 20:41:59 +0200

    Update Fine Print to 0.53a

--- packages/fine_print.rb
+++ packages/fine_print.rb
@@ -1,7 +1,7 @@
 class FinePrint < Kmc::Package
   title 'Fine Print'
   aliases 'fp'
-  url 'http://beta.kerbalstuff.com/mod/81/Fine_Print/download/0.52a'
+  url 'http://beta.kerbalstuff.com/mod/81/Fine_Print/download/0.53a'
 
   def install
     merge_directory 'FinePrint', into: 'GameData'
