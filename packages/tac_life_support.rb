60afca683d42e2275cc332ead70ac5f72985a9bd
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-04 15:26:42 +0200

    Update TAC Life Support to 0.9.0.10

--- packages/tac_life_support.rb
+++ packages/tac_life_support.rb
@@ -1,8 +1,8 @@
 class TACLifeSupport < Kmc::Package
-  title 'TAC Life Support (WIP)'
-  aliases 'tac life support', 'TACLS'
+  title 'TAC Life Support'
+  aliases 'tac life support', 'TACLS', 'tls'
   
-  url 'https://github.com/taraniselsu/TacLifeSupport/releases/download/Release_v0.8/TacLifeSupport_0.8.0.4.zip'
+  url 'https://github.com/taraniselsu/TacLifeSupport/releases/download/v0.9-pre4/TacLifeSupport_0.9.0.10-pre4.zip'
   prerequisites 'module-manager'
 
   def install
