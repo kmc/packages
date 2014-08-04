4f736f9b41422729aeec23da6bb32773d8442e8a
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-04 16:24:08 +0200

    Update Deadly Reentry to 5.2

--- packages/deadly_reentry.rb
+++ packages/deadly_reentry.rb
@@ -1,6 +1,7 @@
 class DeadlyReentry < Kmc::Package
   title 'Deadly Reentry'
-  url 'https://github.com/NathanKell/DeadlyReentry/releases/download/v4.8/DeadlyReentryCont_v4.8.zip'
+  aliases 'dr'
+  url 'https://github.com/NathanKell/DeadlyReentry/releases/download/v5.2/DeadlyReentryCont_v5.2.zip'
   prerequisites 'module-manager'
 
   def install
