c597be6a22601224203bd5fd93f00f7bc2c32b26
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-05 15:17:57 +0200

    Update Modular Kolonization System to 0.19.3

--- packages/modular_kolonization_system.rb
+++ packages/modular_kolonization_system.rb
@@ -1,6 +1,6 @@
 class ModularKolonizationSystem < Kmc::Package
   title 'Modular Kolonization System'
-  url 'https://www.dropbox.com/sh/1fsuzvl35s2gppt/AABOTMlUqHkOUYSxQ41jykPLa/MKS_0.18.8.zip'
+  url 'https://github.com/BobPalmer/MKS/releases/download/0.19.3/MKS_0.19.3.zip'
 
   def install
     merge_directory 'GameData'
