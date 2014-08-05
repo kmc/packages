46be2633197de42b24be03cf464a7e4df564cda7
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-05 15:22:01 +0200

    Add SCANsat7 (RC)

--- packages/scansat.rb
+++ packages/scansat.rb
@@ -7,3 +7,13 @@ class SCANsat < Kmc::Package
     merge_directory 'GameData'
   end
 end
+
+class SCANsat7 < Kmc::Package
+  title 'SCANsat7'
+  aliases 'SCANsatRPM7'
+  url 'https://github.com/S-C-A-N/SCANsat/releases/download/v7.0rc4/SCANsat_v7rc4.zip'
+
+  def install
+    merge_directory 'GameData'
+  end
+end
