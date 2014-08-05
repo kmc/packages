cd242b0f9fb980bde8dcdf2a175b373b8f6a5789
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-05 15:36:36 +0200

    Update Achievements to 1.6.3

--- packages/achievements.rb
+++ packages/achievements.rb
@@ -1,8 +1,9 @@
 class Achievements < Kmc::Package
   title 'Achievements'
-  url 'http://blizzy.de/achievements/Achievements-1.6.0.zip'
+  url 'http://blizzy.de/achievements/Achievements-1.6.3.zip'
+  prerequisites 'toolbar'
 
   def install
-    merge_directory 'Achievements-1.6.0/GameData'
+    merge_directory 'Achievements-1.6.3/GameData/blizzy', into: 'GameData'
   end
 end
