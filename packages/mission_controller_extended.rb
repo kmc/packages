81dcf1722e24072405e2da5f564ebfdec998cf7c
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-07 02:21:47 +0200

    Update Mission Controller 2 to Preview 3 (CURSE)

--- packages/mission_controller_extended.rb
+++ packages/mission_controller_extended.rb
@@ -1,7 +1,7 @@
 class MissionControllerExtended < Kmc::Package
   title 'Mission Controller Extended'
   aliases 'mce'
-  url 'http://www.curse.com/ksp-mods/kerbal/220705-mission-controller-extended'
+  url 'http://kerbal.curseforge.com/ksp-mods/220705-mission-controller-extended'
 
   def install
     merge_directory 'MissionControllerEC', into: 'GameData'
