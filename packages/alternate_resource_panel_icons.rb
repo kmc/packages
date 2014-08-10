070b31b7c6d7df4fa0b247d73dbb21aa6dae86f2
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-10 11:55:27 +0200

    Add KSP Alternate Resource Panel Icons

--- packages/alternate_resource_panel_icons.rb
+++ packages/alternate_resource_panel_icons.rb
@@ -1,5 +1,5 @@
-class AlternateResourcePanel < Kmc::Package
-  title 'KSP Alternate Resource Panel Icons MKS'
+class AlternateResourcePanelIcons < Kmc::Package
+  title 'KSP Alternate Resource Panel Icons'
   aliases 'alternate-resource-panel-icons', 'arp-icons', 'karp-icons'
   url 'https://github.com/Olympic1/ARP_Icons_MKS/archive/master.zip'
 
