b2ee6274dea641144c3d57e6bf816910abc3717c
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-12 23:51:07 +0200

    Add TweakScale like postrequisites to Modular Kolonization System

--- packages/modular_kolonization_system.rb
+++ packages/modular_kolonization_system.rb
@@ -2,6 +2,7 @@ class ModularKolonizationSystem < Kmc::Package
   title 'Modular Kolonization System'
   aliases 'mks'
   url 'https://github.com/BobPalmer/MKS/releases/download/0.19.5/MKS_0.19.5.zip'
+  postrequisites 'TweakScale'
 
   def install
     merge_directory 'GameData'
