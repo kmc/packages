6b4f4028f519a2d04a3c6de2cccbf58f93d36c21
Joris de Vries <j.s.de.vries@gmail.com>
2014-08-21 00:28:44 +0200

    Update Spaceplane Plus to v1.3 for KSP 0.24.2
    This needs https://github.com/kmc/kmc/pull/426 to work.
    
    Made module manager a pre-requisite. Created a custom postrequisite for the Firespitter DLL. In the original download, an outdated Firespitter DLL was situated in a small directory structure, which Kmc could not re-create. https://github.com/kmc/kmc/pull/426 adds directory creation to Kmc, which is needed to install the Firespitter DLL as it is in the zip from SpaceplanePlus.

--- packages/spaceplane_plus.rb
+++ packages/spaceplane_plus.rb
@@ -1,9 +1,21 @@
 class SpacePlanePlus < Kmc::Package
   title 'Spaceplane Plus'
-  url 'https://www.dropbox.com/s/xkaelclfjzzoicn/SpaceplanePlus.zip'
+  url 'https://www.dropbox.com/s/jua0ho5oplq1jie/SpaceplanePlus%20v1.3.zip'
+  prerequisites 'module-manager'
+  postrequisites 'spaceplane-plus-firespitter-dll'
 
   def install
-    merge_directory 'SpaceplanePlus v1.0.0/GameData'
-    merge_directory 'SpaceplanePlus v1.0.0/Ships'
+    merge_directory 'GameData/SpaceplanePlus', into: 'GameData'
+  end
+end
+
+class SpacePlanePlusFirespitterDLL < Kmc::Package
+  title 'Spaceplane Plus Firespitter DLL'
+  url 'https://github.com/snjo/Firespitter/blob/master/For%20release/Firespitter/Plugins/Firespitter.dll?raw=true'
+
+  do_not_unzip!
+
+  def install
+    merge_directory 'Firespitter.dll', into: 'GameData/Firespitter/Plugins/Firespitter.dll'
   end
 end
