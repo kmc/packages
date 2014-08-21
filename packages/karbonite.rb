aece1e6a47d4a2e1bd118aa3bbf1ab1871fe0f46
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-21 17:05:23 +0200

    Update Karbonite to 0.3.2.1

--- packages/karbonite.rb
+++ packages/karbonite.rb
@@ -1,6 +1,6 @@
 class Karbonite < Kmc::Package
   title 'Karbonite'
-  url 'https://github.com/BobPalmer/Karbonite/releases/download/0.3.1/Karbonite_PRE_0.3.1.zip'
+  url 'https://github.com/BobPalmer/Karbonite/releases/download/0.3.2.1/Karbonite_0.3.2.1.zip'
   prerequisites 'module-manager'
 
   def install
