6f1d6a833c037556f2ebd2cdc8f567788da5230d
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-21 17:05:38 +0200

    Update Modular Kolonization System to 0.20.5.1

--- packages/modular_kolonization_system.rb
+++ packages/modular_kolonization_system.rb
@@ -1,7 +1,10 @@
 class ModularKolonizationSystem < Kmc::Package
   title 'Modular Kolonization System'
   aliases 'mks'
-  url 'https://github.com/BobPalmer/MKS/releases/download/0.20.4/MKS_PRE_0.20.4.zip'
+  url 'https://github.com/BobPalmer/MKS/releases/download/0.20.5.1/MKS_20.5.1.zip'
+  prerequisites 'module-manager'
+  postrequisites 'TweakScale'
+  postrequisites 'Karbonite'
 
   def install
     merge_directory 'GameData'
