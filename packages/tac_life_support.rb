717fa2ef8f26a491f2b4b7d94e3ed7d747852d8c
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-25 16:21:01 +0200

    Update TAC Life Support to 0.9.1

--- packages/tac_life_support.rb
+++ packages/tac_life_support.rb
@@ -2,7 +2,7 @@ class TACLifeSupport < Kmc::Package
   title 'TAC Life Support'
   aliases 'tac life support', 'TACLS', 'tls'
   
-  url 'https://github.com/taraniselsu/TacLifeSupport/releases/download/v0.9-pre4/TacLifeSupport_0.9.0.10-pre4.zip'
+  url 'http://kerbal.curseforge.com/ksp-mods/221022-tac-life-support'
   prerequisites 'module-manager'
 
   def install
