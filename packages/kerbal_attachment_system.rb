820fe9df53af2d53a88c5f8d79cbc74f5f18bec4
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-09-01 20:16:52 +0200

    Update Kerbal Attachment System to 0.4.8

--- packages/kerbal_attachment_system.rb
+++ packages/kerbal_attachment_system.rb
@@ -1,7 +1,7 @@
 class KerbalAttachmentSystem < Kmc::Package
   title 'Kerbal Attachment System'
   aliases 'kas'
-  url 'https://nabaal.net/files/ksp/KAS_v0.4.7.zip'
+  url 'http://kerbal.curseforge.com/ksp-mods/223900-kerbal-attachment-system-kas'
 
   def install
     merge_directory 'GameData'
