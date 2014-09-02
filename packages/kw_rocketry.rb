781f71e8ff82c01b55ec2a433e78ebac4b6d3fea
Robin Appelman <icewind@owncloud.com>
2014-09-02 11:55:12 +0200

    update kw rocketry

--- packages/kw_rocketry.rb
+++ packages/kw_rocketry.rb
@@ -2,9 +2,9 @@ class KWRocketry < Kmc::Package
   title 'KW Rocketry'
   aliases 'KW'
   
-  url 'http://www.mediafire.com/download/hxt2kfpctmcmt56/KW+Release+Package+v2.5.6B.zip'
+  url 'http://kerbal.curseforge.com/ksp-mods/220894-kw-rocketry'
 
   def install
-    merge_directory 'KW Release Package v2.5.6B/GameData'
+    merge_directory 'KW Release Package v2.6c (Open this, don\'t extract it)/GameData'
   end
 end
