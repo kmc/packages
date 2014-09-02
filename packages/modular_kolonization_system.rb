b4e943218d1fb33d93134ffea69c801340d9600a
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-09-02 23:41:34 +0200

    Update Modular Kolonization System to 0.20.8

--- packages/modular_kolonization_system.rb
+++ packages/modular_kolonization_system.rb
@@ -1,7 +1,7 @@
 class ModularKolonizationSystem < Kmc::Package
   title 'Modular Kolonization System'
   aliases 'mks'
-  url 'https://github.com/BobPalmer/MKS/releases/download/0.20.7/MKS_0.20.7.zip'
+  url 'https://github.com/BobPalmer/MKS/releases/download/0.20.8/MKS_0.20.8.zip'
   prerequisites 'module-manager'
   postrequisites 'TweakScale'
   postrequisites 'Karbonite'
