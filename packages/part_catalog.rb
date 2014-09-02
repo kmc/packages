b819de9c3bb9061b636811e227bb657408010da0
Robin Appelman <icewind@owncloud.com>
2014-09-02 12:21:47 +0200

    update part catalog

--- packages/part_catalog.rb
+++ packages/part_catalog.rb
@@ -1,6 +1,6 @@
 class PartCatalog < Kmc::Package
   title 'Part Catalog'
-  url 'https://dl.dropboxusercontent.com/u/11467249/PartCatalog/PartCatalog3.0_RC4.zip'
+  url 'https://dl.dropboxusercontent.com/u/11467249/PartCatalog/PartCatalog3.0_RC7.zip'
 
   def install
     merge_directory 'PartCatalog', into: 'GameData'
