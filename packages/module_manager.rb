356118937954a1da90acc6c65d86eb607a3083af
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-09-02 23:42:45 +0200

    Update Module Manager to 2.3.3

--- packages/module_manager.rb
+++ packages/module_manager.rb
@@ -1,9 +1,9 @@
 class ModuleManager < Kmc::Package
   title 'ModuleManager'
   aliases 'module manager'
-  url 'https://ksp.sarbian.com/jenkins/job/ModuleManager/lastSuccessfulBuild/artifact/ModuleManager-2.2.1.zip'
+  url 'https://ksp.sarbian.com/jenkins/job/ModuleManager/lastSuccessfulBuild/artifact/ModuleManager-2.3.3.zip'
 
   def install
-    merge_directory 'ModuleManager.2.2.1.dll', into: 'GameData'
+    merge_directory 'ModuleManager.2.3.3.dll', into: 'GameData'
   end
 end
